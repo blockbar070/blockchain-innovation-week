068f9ca9556fdeb4bd02ae89ecf63cb0a2405f50
bartwr <mail@bartroorda.nl>
2018-05-15 22:46:06 +0200

    about.html -> about.php, so it has the same footer & header as index.php

---
+++ about.php
@@ -0,0 +1,142 @@
+<?php require 'tmpl/head.php'; ?>
+
+    <!--  Main Wrap  -->
+    <div id="main-wrap">
+        <!--  Page Content  -->
+        <div id="page-content" class="header-static">
+            <!--  Page Header  -->
+            <div id="page-header" class="secondary-background">
+                <div class="container">
+                    <div class="row no-margin">
+                        <div class="text">
+                            <h1 class="white">About</h1>
+                            <ul class="breadcrumb white">
+                                <li>
+                                    <a href="#">Home</a>
+                                </li>
+                                <li>About us</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--  END Page Header  -->
+            <div id="home-wrap" class="content-section fullpage-wrap">
+                <div class="container">
+                    <!-- Section Image -->
+                    <div class="row no-margin padding-lg">
+                        <div class="col-md-6 padding-leftright-null">
+                            <div class="text padding-topbottom-null">
+
+                                <h2 class="margin-bottom-null left">Blockchain Innovation Week</h2>
+
+                                <div class="padding-onlytop-sm">
+                                    <p class="margin-bottom margin-md-bottom-null">
+                                        <p>
+                                            This week will be a special one! Two full days where experts share the newest developments and insights in blockchain technologies.
+                                        </p>
+
+                                        <p>
+                                            The first edition of Blockchain Innovation Week will take place on the 24th and 25th of May 2018, The Hague’s first two-day event where blockchain developers share the newest insights in blockchain technologies. Blockchain Innovation Week presents a program of lectures, workshops, interactive panel discussions and time for connecting with others in this emerging field. Are you a developer looking for hands-on practice? Or a business expert who wants to connect with innovative blockchain startups and ready to be inspired by visions of the future? This is your chance to be part of the next era of technology.
+                                        </p>
+
+                                        <p>
+                                            At Blockchain Innovation Week you can meet early adopters: start-ups, developers, makers and consultants with innovative projects. Expect deep-dives and lectures on tokenization, game economics and interactive Q&A sessions where you can ask all your questions.
+                                        </p>
+
+                                        <p>
+                                            This event is organised by Blockbar, consisting of a community that collaborates and exchanges knowledge on blockchain technologies. Do you work on a blockchain or distributed project? Join the Blockbar community, https://www.meetup.com/blockbar/ - you are welcome at our blockchain space in The Hague.
+                                        </p>
+
+                                        <h3>🎫 TICKETS</h3>
+                                        <br>
+
+                                        <p>
+                                            Even our tickets are on the blockchain!
+                                        </p>
+
+                                        <p>
+                                            Buy your tickets: <a href="https://events.guts.tickets/events/104" target="_blank">events.guts.tickets/events/104</a>
+                                        </p>
+                                    </p>
+                                </div>
+
+                                <h2 class="margin-bottom-null left">Blockbar</h2>
+
+                                <div>
+                                    <p class="margin-bottom margin-md-bottom-null">
+
+                                        <p>
+                                          Blockbar is an ecosystem where early-tech adopters, developers, industry experts and blockchain enthusiasts <em>connect</em>.
+                                        </p>
+
+                                        <p>
+                                          We provide a lab where you can develop your ideas on blockchain technology in collaboration with others.
+                                        </p>
+
+                                        <h3>Join the coworking days</h3><br>
+
+                                        <p>
+                                          Every friday from 10am - be welcome!
+                                        </p>
+
+                                        <p>
+                                            <a href="https://www.meetup.com/Ethereum-Cowork-NL/">Meetup</a>
+                                            &nbsp;|&nbsp;
+                                            <a href="https://twitter.com/blockbar070">Twitter</a>
+                                            &nbsp;|&nbsp;
+                                            <a href="https://github.com/bartwr/bldh-site">GitHub</a>
+                                            &nbsp;|&nbsp;
+                                            <a href="https://join.slack.com/t/bitsandblocks/shared_invite/enQtMzA1ODczNTk4NjkzLTZiYTVkNjdiNzljZjQ4Mjk4YzM2ZDc5MDEzMDE1ZDBlNDMyMTg2ZmJiYTkyNjY5NDQ2MmE3ZTIyMDg5OWQxMTM">Slack</a>
+                                        </p>
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 padding-leftright-null padding-md-top">
+                            <div class="text padding-topbottom-null">
+                                <picture>
+                                    <img src="https://images.ctfassets.net/orgovvkppcys/OLAbThTemYiswK8OkOwS4/fde72754bad610bc64cb6ba5d60a0f3c/BC_WS_687x457px_002.gif?w=648&q=100" class="img-responsive shadow" alt="">
+                                </picture>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END Section Image -->
+                </div>
+                <!-- Services Section -->
+<!--                 <div class="light-background">
+                    <div class="container">
+                        <div class="row no-margin padding-lg">
+                            <div class="col-md-4 padding-leftright-null">
+                                <div class="text padding-topbottom-null padding-md-bottom">
+                                    <i class="material-icons color service">card_travel</i>
+                                    <h3 class="margin-bottom-extrasmall">Experience</h3>
+                                    <p class="margin-bottom-null">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4 padding-leftright-null">
+                                <div class="text padding-topbottom-null padding-md-bottom">
+                                    <i class="material-icons color service">trending_up</i>
+                                    <h3 class="margin-bottom-extrasmall">Goals</h3>
+                                    <p class="margin-bottom-null">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4 padding-leftright-null">
+                                <div class="text padding-topbottom-null">
+                                    <i class="material-icons color service">lightbulb_outline</i>
+                                    <h3 class="margin-bottom-extrasmall">Vision</h3>
+                                    <p class="margin-bottom-null">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+
+            </div>
+        </div>
+        <!--  END Page Content -->
+    </div>
+    <!--  Main Wrap  -->
+
+<?php require 'tmpl/foot.php'; ?>
