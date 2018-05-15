a440e3101236587fee3df9846d4328bbe37f4c11
bartwr <mail@bartroorda.nl>
2018-05-16 00:48:22 +0200

    [program overview] Made program overview a module

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                 <div id="page-header" class="secondary-background background-move-effect">
                     <div class="container">
                         <div class="row no-margin">
-                            <a href="https://events.guts.tickets/events/104" target="_blank" class="text" style="display: block;">
+                            <a href="/tickets.php" target="_blank" class="text" style="display: block;">
                                 <h1 class="white">Get your tickets</h1>
                                 <div class="btn-pro white">Buy a day ticket or passe-partout.</div>
                             </a>
@@ -69,36 +69,7 @@
                 </div> -->
                 <!--  END Section Image Background with overlay  -->
 
-                <!-- BEGIN Timeline -->
-                <div class="container">
-                    <!--  Portfolio  -->
-                    <section id="projects" data-isotope="load-simple" class="page">
-                        <!--  Filters  -->
-                        <div class="row no-margin">
-                            <div class="col-sm-12 padding-leftright-null">
-                                <div class="filter-wrap">
-                                    <ul class="col-md-12 filters padding-leftright-null">
-                                        <li data-filter="*" class="is-checked">All</li>
-                                        <li data-filter="tech">Technology</li>
-                                        <li data-filter="business">Business</li>
-                                        <li data-filter="ngo">NGO</li>
-                                        <li data-filter="game">Game</li>
-                                        <li></li>
-                                        <li></li>
-                                        <li></li>
-                                        <li data-filter="lecture">Lecture</li>
-                                        <li data-filter="workshop">Workshop</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!--  END Filters  -->
-                        <div class="projects-items equal three-columns">
-                            <!-- This section will be filled by `assets/js/program.js` -->
-                        </div>
-                    </section>
-                    <!-- END Portfolio -->
-                </div>
+                <?php include 'tmpl/program.php'; ?>
 
             </div>
         </div>
---
+++ tmpl/program.php
@@ -0,0 +1,30 @@
+<!-- BEGIN Timeline -->
+<div class="container">
+    <!--  Portfolio  -->
+    <section id="projects" data-isotope="load-simple" class="page">
+        <!--  Filters  -->
+        <div class="row no-margin">
+            <div class="col-sm-12 padding-leftright-null">
+                <div class="filter-wrap">
+                    <ul class="col-md-12 filters padding-leftright-null">
+                        <li data-filter="*" class="is-checked">All</li>
+                        <li data-filter="tech">Technology</li>
+                        <li data-filter="business">Business</li>
+                        <li data-filter="ngo">NGO</li>
+                        <li data-filter="game">Game</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li data-filter="lecture">Lecture</li>
+                        <li data-filter="workshop">Workshop</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!--  END Filters  -->
+        <div class="projects-items equal three-columns">
+            <!-- This section will be filled by `assets/js/program.js` -->
+        </div>
+    </section>
+    <!-- END Portfolio -->
+</div>
