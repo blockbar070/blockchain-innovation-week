e5423e1e9d249fb722c19af4aa37ab6ff3fa76c8
bartwr <mail@bartroorda.nl>
2018-05-15 22:22:00 +0200

    Created modules for header/footer - From now you'll need PHP on localhost

---
+++ tmpl/foot.php
@@ -0,0 +1,124 @@
+
+    <!--  Footer  -->
+    <footer>
+        <div class="container">
+            <div class="row no-margin">
+                <div class="col-md-3 text">
+                    <div class="logo">
+                        <img src="assets/img/logo_white.png" class="normal" alt="logo">
+                        <img src="assets/img/logo_white@2x.png" class="retina" alt="logo">
+                    </div>
+                    <p>
+                      Blockbar is a The Hague based ecosystem and fysical space where early-tech adopters, developers, industry experts and blockchain ethusiasts connect. Welcome.
+                    </p>
+                    <div>
+                        <ul class="social">
+<!--                             <li>
+                                <a href="">
+                                    <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                                </a>
+                            </li> -->
+                            <li>
+                                <a href="https://twitter.com/blockbar070" target="_blank">
+                                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                                </a>
+                            </li>
+<!--                             <li>
+                                <a href="">
+                                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                                </a>
+                            </li> -->
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 text small">
+                    <h4 class="heading white margin-bottom-small weight-300">Location</h4>
+                    <p>
+                      The Hague Tech (3rd floor)<br>
+                      Juliana van Stolberglaan 10<br>
+                      2595 CL Den Haag<br>
+                    </p>
+<!--                     <h4 class="heading white margin-bottom-small weight-300">Opening times</h4>
+                    <p>
+                      Open on Fridays, see <a href="https://www.meetup.com/blockbar/" target="_blank" style="color: #fff; font-weight: bold;">Meetup</a>
+                    </p> -->
+                </div>
+<!--                 <div class="col-md-2 text small">
+                    <h4 class="heading white margin-bottom-small weight-300">Site Map</h4>
+                    <ul class="info">
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="about.html">About</a>
+                        </li>
+                        <li>
+                            <a href="support.html">Support</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div> -->
+                <!-- <div class="col-md-4 text">
+                    <h4 class="heading white margin-bottom-small weight-300">News Subscribtion</h4>
+                    <p>Subscribe to our newsletter to get all our news in your inbox.</p>
+                    <div id="newsletter-form">
+                        <form class="search-form">
+                            <div class="form-input">
+                                <input type="text" placeholder="Your email ID">
+                                <span class="form-button">
+                                    <button type="button">Sign Up</button>
+                                </span>
+                            </div>
+                        </form>
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </footer>
+<!--     <div id="copy">
+        <div class="container">
+            <div class="row no-margin">
+                <div class="col-md-6 text">
+                    <p>©2017
+                        <a href="#">Febe</a>. All rights reserved. </p>
+                </div>
+                <div class="col-md-6 text text-right">
+                    <ul>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!--  END Footer. Class fixed for fixed footer  -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="assets/js/jquery.min.js"></script>
+
+    <!-- Our own scripts -->
+    <script src="//cdn.jsdelivr.net/npm/ramda@0.25/dist/ramda.min.js"></script>
+    <script src="assets/js/program.js"></script>
+    <script src="assets/js/program-item.js"></script>
+
+    <!-- All js library -->
+    <script src="assets/js/bootstrap/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.flexslider-min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/isotope.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=false"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+    <script src="assets/js/smooth.scroll.min.js"></script>
+    <script src="assets/js/jquery.appear.js"></script>
+    <script src="assets/js/jquery.countTo.js"></script>
+    <script src="assets/js/jquery.scrolly.js"></script>
+    <script src="assets/js/plugins-scroll.js"></script>
+    <script src="assets/js/imagesloaded.min.js"></script>
+    <script src="assets/js/pace.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ tmpl/head.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Blockbar | Open Blockchain Lab The Hague</title>
+    <link rel="icon shortcut" href="assets/img/favicon-16x16.png" sizes="16x16" type="image/png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap-theme.min.css">
+
+    <!-- Custom css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/program.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="assets/css/ionicons.min.css">
+
+    <!-- Flexslider -->
+    <link rel="stylesheet" href="assets/css/flexslider.css">
+
+    <!-- Owl -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+</head>
+
+<body>
+
+    <!--  loader  -->
+    <div id="myloader">
+        <div class="loader">
+            <div class="grid">
+                <div class="cube cube1"></div>
+                <div class="cube cube2"></div>
+                <div class="cube cube3"></div>
+                <div class="cube cube4"></div>
+                <div class="cube cube5"></div>
+                <div class="cube cube6"></div>
+                <div class="cube cube7"></div>
+                <div class="cube cube8"></div>
+                <div class="cube cube9"></div>
+            </div>
+        </div>
+    </div>
+
+    <!--  Header & Menu  -->
+    <header id="header">
+
+        <div class="top-nav">
+            <!--  Header Logo  -->
+            <div id="logo">
+                <a class="navbar-brand" href="index.html">
+                    <img src="assets/img/logo.png" class="normal" alt="logo">
+                    <img src="assets/img/logo@2x.png" class="retina" alt="logo">
+                </a>
+            </div>
+            <!--  END Header Logo  -->
+            <div class="secondary-menu">
+                <ul>
+                    <li class="mail">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                        <a href="mailto:team@blockbar.nl">team@blockbar.nl</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <nav class="navbar navbar-default">
+            <!--  Classic menu, responsive menu classic  -->
+            <div id="menu-classic">
+                <div class="menu-holder">
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="programme.html" class="active-item">Programme</a>
+                        </li>
+                        <li>
+                            <a href="about.html">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!--  END Classic menu, responsive menu classic  -->
+            <!--  Button for Responsive Menu Classic  -->
+            <div id="menu-responsive-classic">
+                <div class="menu-button">
+                    <span class="bar bar-1"></span>
+                    <span class="bar bar-2"></span>
+                    <span class="bar bar-3"></span>
+                </div>
+            </div>
+            <!--  END Button for Responsive Menu Classic  -->
+            <div class="secondary-menu-mobile">
+                <ul>
+                    <li class="mail">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                        <a href="mailto:team@blockbar.nl">team@blockbar.nl</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!--  END Header & Menu  -->
