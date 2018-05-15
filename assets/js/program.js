033808aae8310195936a1a0a5ba2948093f1691a
bartwr <mail@bartroorda.nl>
2018-05-15 12:04:45 +0200

    [program overview] Load tags from Wordpress

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,13 +1,29 @@
 var $projectItems = $('#projects .projects-items');
 
+var getTagsClasses = function(item) {
+  let tags = '';
+  R.map(function(tag) {
+    tags += ' tag-' + tag.name
+  }, item.tags_full)
+  return tags;
+}
+
+var getTagsString = function(item) {
+  let tags = '';
+  R.map(function(tag) {
+    tags += (tags.length > 0 ? ', ' : '') + tag.name
+  }, item.tags_full)
+  return tags;
+}
+
 var renderProgramItem = function(item) {
   $projectItems.append('\
-    <div class="single-item one-item business ngo workshop">\
+    <div class="single-item one-item '+getTagsClasses(item)+'">\
         <div class="item">\
             <img src="'+item.featured_image_url+'" alt="">\
             <div class="content">\
                 <h3>'+item.title.rendered+'</h3>\
-                <span class="categories">Business, NGO, Workshop</span>\
+                <span class="categories">'+getTagsString(item)+'</span>\
             </div>\
             <a href="pg_pil.html" class="link"></a>\
         </div>\
