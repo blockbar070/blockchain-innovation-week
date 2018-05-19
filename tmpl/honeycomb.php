56406c3f5093919c92d42dacd42d8d17e2aab594
bartwr <mail@bartroorda.nl>
2018-05-19 20:43:39 +0200

    Randomized honeycomb logos

--- tmpl/honeycomb.php
+++ tmpl/honeycomb.php
@@ -1,98 +1,135 @@
-<img src="https://c1.staticflickr.com/1/852/26785490577_e4ecd65dcc_c.jpg" class="img-responsive show-on-mobile" alt="">
+<?php
 
-<ul class="honeycomb hide-on-mobile">
-  <?php $btcUrls = array(
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<img src="https://c1.staticflickr.com/1/852/26785490577_e4ecd65dcc_c.jpg" class="img-responsive show-on-mobile">
+
+<?php
+
+$logos = [];
+
+$logos[] = [
+  'name' => 'btc',
+  'title' => 'Bitcoin',
+  'urls' => [
     './lineup/the-huge-potential-for-bitcoin-komodo',
     './lineup/what-is-blockchain-why-should-i-care',
     './lineup/ama-met-robert-reinder-nederhoed-en-thomas-kemmere'
-  ); ?>
-  <li class="item">
-    <a href="<?php echo $btcUrls[rand(0, sizeof($btcUrls)-1)]; ?>" title="Bitcoin">
-      <img src="./assets/img/logo/btc.png" alt="Bitcoin">
-    </a>
+  ],
+];
 
-  </li>
-  <li class="item">
-    <a href="https://espeoblockchain.com/" title="Espeo Blockchain" target="_blank">
-      <img src="./assets/img/logo/Espeo.png" alt="Logo: Espeo Blockchain">
-    </a>
-  </li>
-  <li class="item">
-    <a href="https://www.thehaguetech.com/" target="_blank" title="The Hague Tech">
-      <img src="./assets/img/logo/THT.png" alt="The Hague Tech">
-    </a>
-  </li>
-  <li class="item">
-    <a href="https://impactcity.nl/" target="_blank" title="Impact City">
-      <img src="./assets/img/logo/impactcity.png" alt="Impact City">
-    </a>
-  </li>
-  <?php $rchainUrls = array(
+$logos[] = [
+  'name' => 'espeo',
+  'title' => 'Espeo Blockchain',
+  'urls' => ['https://espeoblockchain.com/'],
+];
+
+$logos[] = [
+  'name' => 'tht',
+  'title' => 'The Hague Tech',
+  'urls' => ['https://www.thehaguetech.com/'],
+];
+
+$logos[] = [
+  'name' => 'impactcity',
+  'title' => 'Impact City',
+  'urls' => ['https://impactcity.nl/']
+];
+
+$logos[] = [
+  'name' => 'rchain',
+  'title' => 'RChain',
+  'urls' => [
     './lineup/rchain-workshop-coding-smart-contracts',
     './lineup/concurrent-computation-for-a-secure-scalable-and-sustainable-blockchain'
-  ); ?>
-  <li class="item">
-    <a href="<?php echo $rchainUrls[rand(0, sizeof($rchainUrls)-1)]; ?>" title="RChain">
-      <img src="./assets/img/logo/rchain.png" alt="06">
-    </a>
-  </li>
-  <?php $zcashUrls = array(
+  ]
+];
+
+$logos[] = [
+  'name' => 'zcash',
+  'title' => 'ZCash',
+  'urls' => [
     './lineup/zksnarks-what-do-they-do'
-  ); ?>
-  <li class="item">
-    <a href="<?php echo $zcashUrls[rand(0, sizeof($zcashUrls)-1)]; ?>" title="ZCash">
-      <img src="./assets/img/logo/zcash.png" alt="07">
-    </a>
-  </li>
-  <li class="item">
-    <a href="https://cryptopepes.io" target="_blank" title="CryptoPepes">
-      <img src="./assets/img/logo/pepe2.png" alt="CryptoPepes">
-    </a>
-  </li>
-  <li class="item">
-    <a title="Monero"><img src="./assets/img/logo/monero.png" alt=""></a>
-  </li>
-  <?php $komodoUrls = array(
+  ]
+];
+
+$logos[] = [
+  'name' => 'pepe',
+  'title' => 'CryptoPepes',
+  'urls' => [
+    'https://cryptopepes.io'
+  ]
+];
+
+$logos[] = [
+  'name' => 'komodo',
+  'title' => 'Komodo',
+  'urls' => [
     './lineup/barterdex-workshop',
     './lineup/the-huge-potential-for-bitcoin-komodo'
-  ); ?>
-  <li class="item">
-    <a href="<?php echo $komodoUrls[rand(0, sizeof($komodoUrls)-1)]; ?>" title="Komodo">
-      <img src="./assets/img/logo/komodo.png" alt="Komodo">
-    </a>
-  </li>
-  <li class="item">
-    <a href="./lineup/why-how-to-use-hardware-wallets">
-      <img src="./assets/img/logo/trezor.png" alt="Trezor">
-    </a>
-  </li>
-  <?php $steemitUrls = array(
+  ]
+];
+
+$logos[] = [
+  'name' => 'trezor',
+  'title' => 'Trezor',
+  'urls' => ['./lineup/why-how-to-use-hardware-wallets']
+];
+
+$logos[] = [
+  'name' => 'steemit',
+  'title' => 'Steemit',
+  'urls' => [
+    './lineup/why-how-to-use-hardware-wallets',
     './lineup/steemit-earn-money-by-using-social-media',
     './lineup/your-first-steps-on-steemit'
-  ); ?>
-  <li class="item">
-    <a href="<?php echo $komodoUrls[rand(0, sizeof($komodoUrls)-1)]; ?>" title="Steemit">
-      <img src="./assets/img/logo/steemit.png" alt="Steemit">
-    </a>
-  </li>
-  <li class="item">
-    <a href="./lineup/eos-the-governed-chain" title="EOS">
-      <img src="./assets/img/logo/eos.png" alt="EOS">
-    </a>
-  </li>
-  <li class="item">
-    <a href="https://d.tube/" target="_blank">
-      <img src="./assets/img/logo/dtube.png" alt="DTube">
-    </a>
-  </li>
-  <?php $ethUrls = array(
+  ]
+];
+
+$logos[] = [
+  'name' => 'eos',
+  'title' => 'EOS',
+  'urls' => ['./lineup/eos-the-governed-chain']
+];
+
+$logos[] = [
+  'name' => 'dtube',
+  'title' => 'DTube',
+  'urls' => ['https://d.tube/']
+];
+
+$logos[] = [
+  'name' => 'ethereum',
+  'title' => 'Ethereum',
+  'urls' => [
     './lineup/cryptopepes-a-brief-introduction',
     './lineup/ethergoo-and-idle-games',
     './lineup/human-readable-smart-contracts'
-  ); ?>
-  <li class="item">
-    <a href="<?php echo $ethUrls[rand(0, sizeof($ethUrls)-1)]; ?>" title="Ethereum">
-      <img src="./assets/img/logo/ethereum.png" alt="Ethereum">
-    </a>
-  </li>
+  ]
+];
+
+function getAllLogos($logos) {
+  // Randomize logos
+  shuffle($logos);
+  // Show all items
+  foreach ($logos as $key => $logo) {
+    $randomUrl = $logo['urls'][rand(0, sizeof($logo['urls'])-1)];
+    ?>
+    <li class="item">
+      <a href="<?php echo $randomUrl; ?>"
+         title="<?php echo $logo['title']; ?>"
+         target="<?php echo strpos($randomUrl, 'http') >= -1 ? '_blank' : '_self'; ?>">
+        <img src="./assets/img/logo/<?php echo $logo['name']; ?>.png"
+          alt="<?php echo $logo['title']; ?> logo">
+      </a>
+    </li>
+  <?php }
+}
+
+?>
+<ul class="honeycomb hide-on-mobile">
+  <?php getAllLogos($logos); ?>
 </ul>
